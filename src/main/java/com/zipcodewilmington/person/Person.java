0f2b53ac0631462c5ca52cc369a4f06aff2c45a9
Joey <joey@Zipcoders-MacBook-Pro-3.local>
2023-11-05 16:33:02 -0500

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,49 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private boolean isAMom;
+    private String address;
+    private String email;
+
+    public Person(String name, int age, String email, boolean isAMom, String address) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.isAMom = isAMom;
+        this.address = address;
+    }
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
